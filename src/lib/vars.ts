d40cc0f0782fee5b15b5634556d9313f07f31a8c
Lucas Hang <lucas.hhang@gmail.com>
2022-07-05 00:09:46 -0300

    feat: add prod peer server

--- src/lib/vars.ts
+++ src/lib/vars.ts
@@ -1,2 +1,2 @@
-export const GUN_PEERS = ['http://localhost:3030/gun'];
+export const GUN_PEERS = ['http://localhost:3030/gun', 'https://task-scorer-server.herokuapp.com/gun'];
 export const GUN_PARTICIPANTS_KEY = 'participants';
