1d274d666c960e54b6f88f1206fcce91220d626d
Lucas Hang <lucas.hhang@gmail.com>
2022-08-04 21:21:04 -0300

    fix: uncomment prod peer

--- src/lib/vars.ts
+++ src/lib/vars.ts
@@ -1,6 +1,6 @@
 export const GUN_PEERS = [
-	'http://localhost:80/gun'
-	// 'https://task-scorer-server.herokuapp.com/gun'
+	'http://localhost:80/gun',
+	'https://task-scorer-server.herokuapp.com/gun'
 ];
 export const GUN_PARTICIPANTS_COUNTER_KEY = 'participantsCounter';
 export const GUN_PARTICIPANTS_KEY = 'participants';
