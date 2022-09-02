a26add552e4ece18b16201c8d908af3b6745cfda
Lucas Hang <lucas.hhang@gmail.com>
2022-09-02 19:40:35 -0300

    feat: does not save gun data on local storage anymore :D

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
 import Gun from 'gun/gun.js';
 import { GUN_PEERS } from './vars';
 
-const gun = Gun({ peers: GUN_PEERS });
+const gun = Gun({ peers: GUN_PEERS, localStorage: false });
 
 export default gun;
